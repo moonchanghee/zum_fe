661a4c1e22588afdfd01ecce34b0ca755eed68ed
moonchanghee <ckdgml228@gmail.com>
2022-01-28 20:29:28 +0900

    item update

--- src/components/Items.js
+++ src/components/Items.js
@@ -9,20 +9,28 @@ export default class Items extends Component {
   template () {
     const { items } = this.$state;
     return `
-      <ul>
+    <p>메인 페이지</p>  
+    <ul>
         ${items.map(item => `<li>${item}</li>`).join('')}
       </ul>
-      <button class = "btn1">추가</button>
+      <button id = "btn2"  route = "/home" >작성</button> 
+      <button id = "btn1">추가111</button>
     `
   }
 
   setEvent () {
     const {items} = this.$state;
+    document.getElementById("btn1").addEventListener('click', () => {
     // this.$target.getElementById("btn1").addEventListener('click', () => {
-    this.$target.querySelector('button').addEventListener('click', () => {
+    // this.$target.querySelector('button').addEventListener('click', () => {
     fetch("http://localhost:3001/get")
     .then((response) => response.json())
     .then((data) => this.setState({items:[...items,data.data]}));
     });
+
+    document.getElementById("btn2").addEventListener('click', (e) => {
+      console.log(e)
+      });
   }
 }
+
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,10 @@
 module.exports = { 
     mode : 'development',
-    entry: __dirname + '/src/index.js', 
+    // entry: __dirname + '/src/index.js', 
+    // entry: {
+    //     // router: './router.js',
+    //     app: '/src/index.js'
+    //   },
     output: { 
         path: __dirname + '/public', 
         filename: 'bundle.js' 
